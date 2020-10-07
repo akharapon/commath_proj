f1811608e122b588adcb093e409b6f6519805484
akharapon <akharapon.ju.61@ubu.ac.th>
2020-10-07 23:18:05 +0700

    Updates

---
+++ precache-manifest.ea6565df40f69f1f204ec052cb011cb8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "617232fd09ad5bfe40202673366928c8",
+    "url": "/commath_proj/index.html"
+  },
+  {
+    "revision": "1fd436da9c98492fba9f",
+    "url": "/commath_proj/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fe9bfa9a746d20cf0f16",
+    "url": "/commath_proj/static/js/2.98d3735c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/commath_proj/static/js/2.98d3735c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fd436da9c98492fba9f",
+    "url": "/commath_proj/static/js/main.dfc429de.chunk.js"
+  },
+  {
+    "revision": "5192cbbf04d3464941db",
+    "url": "/commath_proj/static/js/runtime-main.580e87e7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/commath_proj/static/media/logo.5d5d9eef.svg"
+  }
+]);
